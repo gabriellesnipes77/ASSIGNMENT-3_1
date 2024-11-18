297386a9c8292d95e9dbb766450397e16d517ee6
BIG DON <BIG DON@MSI>
2024-11-18 09:56:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ASSIGNMENT_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GetEvenNumbers");
+        }
+       
+            static string GetEvenNumbers(int numAmount)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 2; i < 100; i = +2)
+                {
+                    sb.Append(i);
+                    if (i < 98)
+                        sb.Append(", ");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
